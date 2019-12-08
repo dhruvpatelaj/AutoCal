a2cbd96373837389edd07cb9a5e56960c23e9aa2
Jorge Ortiz <49216265+joor2163@users.noreply.github.com>
2019-12-07 22:50:47 -0700

    Update background.js
    Added the code that will detect the title of the part that the user highlights. It uses regex to find certain keywords and takes that word and the word before it and makes that as the title.

--- background.js
+++ background.js
@@ -17,7 +17,6 @@ var time;
 var endTime;
 var loc;
 var description;
-var keyWords =  ["tomorrow", "in", "on", "friday","monday","have"];//Just an array that is being used to test
 //....
 
 /** BACKEND  */
@@ -93,26 +92,49 @@ function eventRecognition(selection)
 
 function titleFinder(selection) //this works but it is still some problems
 {
+  // alert(selection)
   var temp = "";
+  var temp2  ="";
   var flag = false;
+  // var flag = false;
   title = "";
-  for(var i = 0; i<selection.length; i++){
-    if(selection[i] != " "){
-      temp = temp + selection[i];
-    }else if(selection[i] == " "){
-      for(var k = 0; k < keyWords.length; k++){
-        if(temp.toLowerCase() == keyWords[k]){
-          flag = true;
-          break;
-        }
-      }
-      if(flag == true){
-        break;
-      }
-      title = title + " " + temp;
-      temp = "";
+  // for(var i = 0; i<selection.length; i++){
+  //   if(selection[i] != ' '){
+  //     temp = temp + selection[i];
+  //   }else if(selection[i] == ' '){
+  //     for(var k = 0; k < keyWords.length; k++){
+  //       if(temp.toLowerCase() == keyWords[k]){
+  //         flag = true;
+  //         break;
+  //       }
+  //     }
+  //     if(flag == true){
+  //       break;
+  //     }
+  //     title = title + " " + temp;
+  //     temp = "";
+  //   }
+  // }
+
+  //
+  var select = selection.toLowerCase();
+  var key = select.search(/(Club|Lesson|Deadline|Application|Party|Event|Dance|Show|Performance|Concert|Cook|Cooking|Clean|Cleaning|Wash|Washing|Print|Work|Internship|Job|Review|Study|Class|Recitation|Lecture|Lab|Drive|Commute|Fair|Playdate|Interview|Celebration|Hike|Bike|Football|Soccer|Ride|Tutor|Tutoring|Train|Training|Work|Mandatory|Optional|Volunteer|Anniversary|Birthday|Write|Paint|Draw|Practice|Review|Midterm|Test|exam|practicum|quiz|Final|Title|Event|Vacation|trip|retreat|camp|Breakfast|Lunch|Dinner|break)/i);
+  var counter = key;
+  while(flag == false){
+    if(select[counter] == ' '){
+      break;
+    }
+    temp = temp + select[counter];
+    counter++;
+  }
+  var res = select.split(" ");
+  for(var i=0;i < res.length;i++){
+    if(temp == res[i]){
+      title = res[i-1] + " " + res[i]
+      break;
     }
   }
+
 }
 
 
