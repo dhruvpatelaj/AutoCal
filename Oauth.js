74ad5e9f854ed956222524c8bd171e5b9617bbdb
Dhruv Patel <dhruvpatel.aj@gmail.com>
2019-12-09 17:21:47 -0700

    some stuff

--- Oauth.js
+++ Oauth.js
@@ -21,7 +21,34 @@ function militaryToStanard(time){
     
     timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
     timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
-    timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+    timeValue += (hours >= 12) ? " pm" : " am";  // get AM/PM
+
+    return timeValue;
+}
+
+function militaryToStanardTwo(time){
+
+    time = time.split(':'); // convert to array
+
+    // fetch
+    var hours = Number(time[0]);
+    var minutes = Number(time[1]);
+    var seconds = Number(time[2]);
+
+    // calculate
+    var timeValue;
+
+    if (hours > 0 && hours <= 12) {
+    timeValue= "" + hours;
+    } else if (hours > 12) {
+    timeValue= "" + (hours - 12);
+    } else if (hours == 0) {
+    timeValue= "12";
+    }
+    
+    timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+    timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
+    //timeValue += (hours >= 12) ? " pm" : " am";  // get AM/PM
 
     return timeValue;
 }
@@ -36,59 +63,59 @@ function removeElement(elementId) {
     
 window.onload = function() {
 
-    chrome.storage.sync.get(['login'], function(result) {
-        if(result.login){
-            removeElement("Oauth")
-        }
-    });
+    // chrome.storage.sync.get(['login'], function(result) {
+    //     if(result.login){
+    //         removeElement("Oauth")
+    //     }
+    // });
 
     
-    document.getElementById('Oauth').addEventListener('click', function () {
-        chrome.identity.getAuthToken({interactive: true}, function (token) {
-            const headers = new Headers({
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json'
-            });
+    // document.getElementById('Oauth').addEventListener('click', function () {
+    //     chrome.identity.getAuthToken({interactive: true}, function (token) {
+    //         const headers = new Headers({
+    //             'Authorization': 'Bearer ' + token,
+    //             'Content-Type': 'application/json'
+    //         });
 
-            const queryParams = {headers};
+    //         const queryParams = {headers};
 
-            value = true;
-            chrome.storage.sync.set({'login': value}, function () {
+    //         value = true;
+    //         chrome.storage.sync.set({'login': value}, function () {
                 
-            });
+    //         });
 
 
-            fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
-                .then((response) => response.json()) // Transform the data into json
-                .then(function (data) {
-                    console.log(data);
-                });
+    //         fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+    //             .then((response) => response.json()) // Transform the data into json
+    //             .then(function (data) {
+    //                 console.log(data);
+    //             });
 
-            var makeQuerystring = params =>
-                Object.keys(params)
-                    .map(key => {
-                        return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
-                    })
-                    .join("&");
+    //         var makeQuerystring = params =>
+    //             Object.keys(params)
+    //                 .map(key => {
+    //                     return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
+    //                 })
+    //                 .join("&");
 
-            fetch(
-                "https://www.googleapis.com/calendar/v3/calendars/primary/events/quickAdd",
-                {
-                    method: "post",
-                    body: makeQuerystring({
-                        text: "Appointment at Somewhere on December 15th 10am-10:25am"
-                    }),
-                    headers: {
-                        'Authorization': 'Bearer ' + token,
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    }
-                }
-            )
-                .then(res => res.json())
-                .then(json => console.log(json));
-        })
+    //         fetch(
+    //             "https://www.googleapis.com/calendar/v3/calendars/primary/events/quickAdd",
+    //             {
+    //                 method: "post",
+    //                 body: makeQuerystring({
+    //                     text: "Appointment at Somewhere on December 15th 10am-10:25am"
+    //                 }),
+    //                 headers: {
+    //                     'Authorization': 'Bearer ' + token,
+    //                     "Content-Type": "application/x-www-form-urlencoded"
+    //                 }
+    //             }
+    //         )
+    //             .then(res => res.json())
+    //             .then(json => console.log(json));
+    //     })
 
-    });
+    // });
 
 
 
@@ -146,7 +173,7 @@ window.onload = function() {
                 .then(res => res.json())
                 .then(json => console.log(json));
         })
-        window.close();
+        //window.close();
         
     });
 };
