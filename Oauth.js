82755635dcdba81a13ad6fd72759b9098ac494f3
Zhouming Sun <pokemmlink@gmail.com>
2019-12-13 14:32:58 -0700

    removed login button

--- Oauth.js
+++ Oauth.js
@@ -38,60 +38,6 @@ function removeElement(elementId) {
 
 window.onload = function() {
 
-    // chrome.storage.sync.get(['login'], function(result) {
-    //     if(result.login){
-    //         removeElement("Oauth")
-    //     }
-    // });
-
-
-    // document.getElementById('Oauth').addEventListener('click', function () {
-    //     chrome.identity.getAuthToken({interactive: true}, function (token) {
-    //         const headers = new Headers({
-    //             'Authorization': 'Bearer ' + token,
-    //             'Content-Type': 'application/json'
-    //         });
-
-    //         const queryParams = {headers};
-
-    //         value = true;
-    //         chrome.storage.sync.set({'login': value}, function () {
-
-    //         });
-
-
-    //         fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
-    //             .then((response) => response.json()) // Transform the data into json
-    //             .then(function (data) {
-    //                 console.log(data);
-    //             });
-
-    //         var makeQuerystring = params =>
-    //             Object.keys(params)
-    //                 .map(key => {
-    //                     return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
-    //                 })
-    //                 .join("&");
-
-    //         fetch(
-    //             "https://www.googleapis.com/calendar/v3/calendars/primary/events/quickAdd",
-    //             {
-    //                 method: "post",
-    //                 body: makeQuerystring({
-    //                     text: "Appointment at Somewhere on December 15th 10am-10:25am"
-    //                 }),
-    //                 headers: {
-    //                     'Authorization': 'Bearer ' + token,
-    //                     "Content-Type": "application/x-www-form-urlencoded"
-    //                 }
-    //             }
-    //         )
-    //             .then(res => res.json())
-    //             .then(json => console.log(json));
-    //     })
-
-    // });
-
 
 
     document.getElementById('submit').addEventListener('click', function () {
